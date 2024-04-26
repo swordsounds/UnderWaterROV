4315303cbe867b765bb77ee0bb3ea0f50c80b605
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-04-25 20:29:07 -0700

    For lorenzo

--- 1_TESTING/TestingFileForMotors.py
+++ 1_TESTING/TestingFileForMotors.py
@@ -4,14 +4,28 @@ from Phidget22.Devices.CurrentInput import *
 
 dcMotor0 = DCMotor()
 dcMotor1 = DCMotor()
-
-currentInput0 = CurrentInput()
-currentInput1 = CurrentInput()
+dcMotor2 = DCMotor()
+dcMotor3 = DCMotor()
+dcMotor4 = DCMotor()
+dcMotor5 = DCMotor()
 
 dcMotor0.setDeviceSerialNumber(487701)
 dcMotor0.setChannel(0)
 dcMotor1.setDeviceSerialNumber(487701)
 dcMotor1.setChannel(1)
+
+dcMotor2.setDeviceSerialNumber(487701)
+dcMotor2.setChannel(0)
+dcMotor3.setDeviceSerialNumber(487701)
+dcMotor3.setChannel(1)
+
+dcMotor4.setDeviceSerialNumber(487701)
+dcMotor4.setChannel(0)
+dcMotor5.setDeviceSerialNumber(487701)
+dcMotor5.setChannel(1)
+
+currentInput0 = CurrentInput()
+currentInput1 = CurrentInput()
 
 currentInput0.setDeviceSerialNumber(487701)
 currentInput0.setChannel(0)
@@ -40,9 +54,49 @@ def motor_two():
 
 	currentInput1.openWaitForAttachment(1000)
 	# currentInput1.setOnCurrentChangeHandler(onCurrentChange)
+def motor_three():
+	dcMotor2.openWaitForAttachment(1000)
+	dcMotor2.setAcceleration(19.4)
+	dcMotor2.setTargetVelocity(1)
+
+	currentInput1.openWaitForAttachment(1000)
+	# currentInput1.setOnCurrentChangeHandler(onCurrentChange)
+def motor_four():
+	dcMotor3.openWaitForAttachment(1000)
+	dcMotor3.setAcceleration(19.4)
+	dcMotor3.setTargetVelocity(1)
+
+	currentInput1.openWaitForAttachment(1000)
+	# currentInput1.setOnCurrentChangeHandler(onCurrentChange)
+def motor_five():
+	dcMotor4.openWaitForAttachment(1000)
+	dcMotor4.setAcceleration(19.4)
+	dcMotor4.setTargetVelocity(1)
+
+	currentInput1.openWaitForAttachment(1000)
+	# currentInput1.setOnCurrentChangeHandler(onCurrentChange)
+def motor_six():
+	dcMotor5.openWaitForAttachment(1000)
+	dcMotor5.setAcceleration(19.4)
+	dcMotor5.setTargetVelocity(1)
+
+	currentInput1.openWaitForAttachment(1000)
+	# currentInput1.setOnCurrentChangeHandler(onCurrentChange)
 
 def motor_one_close():
 	dcMotor0.close()
 
 def motor_two_close():
 	dcMotor1.close()
+
+def motor_three_close():
+	dcMotor2.close()
+
+def motor_four_close():
+	dcMotor3.close()
+
+def motor_five_close():
+	dcMotor4.close()
+
+def motor_six_close():
+	dcMotor5.close()
