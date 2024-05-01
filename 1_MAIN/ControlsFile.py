98dbca58bd2673fa8d6becfba938cf20db07a8dc
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-05-01 16:21:33 -0230

    Controlfile updates

--- 1_MAIN/ControlsFile.py
+++ 1_MAIN/ControlsFile.py
@@ -8,44 +8,79 @@ scroll_value = 100
 
 def on_press(key):
     try:
-        if key.char == 'w':
-             mtr.motor_one(throttle=1)
+        if key.char == 'shift':
+             mtr.motor_four(throttle=1)
              mtr.motor_two(throttle=1)
-        elif key.char == 's':
-            mtr.motor_one(throttle=-1)
+        elif key.char == 'ctrl':
+            mtr.motor_four(throttle=-1)
             mtr.motor_two(throttle=-1)
-        # elif key.char == 'a':
-        #     mtr.motor_one(throttle=1)
-        #     mtr.motor_three(throttle=1)
-        #     mtr.motor_five(throttle=1)
-        #     mtr.motor_six(throttle=1)
-        # elif key.char == 'd':
-        #     mtr.motor_one(throttle=1)
-        #     mtr.motor_three(throttle=1)
-        #     mtr.motor_five(throttle=1)
-        #     mtr.motor_six(throttle=1)
+        elif key.char == 'q':
+            mtr.motor_five(throttle=1)
+            mtr.motor_six(throttle=-1)
+            mtr.motor_one(throttle=1)
+            mtr.motor_three(throttle=-1)
+        elif key.char == 'e':
+            mtr.motor_five(throttle=-1)
+            mtr.motor_six(throttle=1)
+            mtr.motor_one(throttle=-1)
+            mtr.motor_three(throttle=1)
+        elif key.char == 'w':
+            mtr.motor_six(throttle=1)
+            mtr.motor_five(throttle=1)
+        elif key.char == 's':
+            mtr.motor_one(throttle=1)
+            mtr.motor_three(throttle=1)
+        elif key.char == 'a':
+            mtr.motor_one(throttle=1)
+            mtr.motor_three(throttle=-1)
+            mtr.motor_five(throttle=-1)
+            mtr.motor_six(throttle=1)
+        elif key.char == 'd':
+            mtr.motor_one(throttle=-1)
+            mtr.motor_three(throttle=1)
+            mtr.motor_five(throttle=1)
+            mtr.motor_six(throttle=-1)
     except Exception as e:
         print(f"Raised exception:{e}")
 
 def on_release(key):
-    if key.char == 'w':
-        mtr.motor_one_close()
-        mtr.motor_two_close()
-    elif key.char == 's':
-        mtr.motor_one_close()
-        mtr.motor_two_close()
-    elif key == keyboard.Key.esc:
-        return False
-    # elif key.char == 'a':
-    #     mtr.motor_three_close()
-    #     mtr.motor_four_close()
-    #     mtr.motor_five_close()
-    #     mtr.motor_six_close()
-    # elif key.char == 'd':
-    #     mtr.motor_three_close()
-    #     mtr.motor_four_close()
-    #     mtr.motor_five_close()
-    #     mtr.motor_six_close()
+    try:
+        if key.char == 'shift':
+            mtr.motor_four_close()
+            mtr.motor_two_close()
+        elif key.char == 'ctrl':
+            mtr.motor_four_close()
+            mtr.motor_two_close()
+        elif key.char == 'q':
+            mtr.motor_five_close()
+            mtr.motor_six_close()
+            mtr.motor_one_close()
+            mtr.motor_three_close()
+        elif key.char == 'e':
+            mtr.motor_five_close()
+            mtr.motor_six_close()
+            mtr.motor_one_close()
+            mtr.motor_three_close()
+        elif key.char == 'w':
+            mtr.motor_one_close()
+            mtr.motor_two_close()
+        elif key.char == 's':
+            mtr.motor_one_close()
+            mtr.motor_two_close()
+        elif key.char == 'a':
+            mtr.motor_three_close()
+            mtr.motor_four_close()
+            mtr.motor_five_close()
+            mtr.motor_six_close()
+        elif key.char == 'd':
+            mtr.motor_three_close()
+            mtr.motor_four_close()
+            mtr.motor_five_close()
+            mtr.motor_six_close()
+        elif key == keyboard.Key.esc:
+            return False
+    except Exception as e:
+        print(e)
     
 # def on_scroll(x, y, dx, dy):
 #     global scroll_value
@@ -66,5 +101,4 @@ def main():
     # listenerMouse.start()
 
 if __name__ == "__main__":
-    while True:
-        main()
+     main()
