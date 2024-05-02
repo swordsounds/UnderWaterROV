aeef632053b763afd10cbaad0fd0739d7f96cdb5
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-05-02 13:27:14 -0230

    Stuff

--- 1_TESTING/GeneralTesting.py
+++ 1_TESTING/GeneralTesting.py
@@ -1,24 +1,46 @@
 import matplotlib.pyplot as plt
-from matplotlib import ticker
-import numpy as np
+import pandas as pd
+import csv
+from datetime import datetime
 
-#- define the data
-x = np.linspace(0., 1., 100)
-y = np.random.randn(len(x))
+now = datetime.now()
 
-#- plot it
-ax = plt.gca()
-ax.plot(x, y, 'k')
+current_time = now.strftime("%H:%M:%S") 
 
-#- define your own locator based on ticker.LinearLocator
-class MyLocator(ticker.LinearLocator):
-   def tick_values(self, vmin, vmax):
-       "vmin and vmax are the axis limits, return the tick locations here"
-       return [vmin, 0.5 * (vmin + vmax), vmax]
 
-#- initiate the locator and attach it to the current axis
-ML = MyLocator()
-ax.yaxis.set_major_locator(ML)
+data = {
+  "time": [current_time, current_time, current_time],
+  "mtr1": [420, 380, 390],
+  "mtr2": [420, 380, 390],
+  "mtr3": [420, 380, 390],
+  
+}
 
+data['time'].append(current_time)
+data['mtr1'].append(500)
+data['mtr2'].append(500)
+data['mtr3'].append(500)
+
+myvar = pd.DataFrame(data)
+
+myvar.to_csv('data.csv', encoding='utf-8', index=False)
+
+df = pd.read_csv('data.csv')
+
+print(df)
+# df = df.reset_index(drop=True)
+# myvar = pd.Series(calories, index = dict(list(calories.items())[:2]))
+# print(myvar)
+# print(myvar.loc['mtr1'])
+# print(myvar['calories'])
+# print(myvar.loc[0])
+# print(myvar.loc[[0,1]])
+
+# csv_writer.writerow(myvar.loc[0])
+# myvar.plot(x='time', y='current')
+fig, axes = plt.subplots()
+
+axes.plot(df['time'][-30:], df['mtr1'][-30:])
+# axes = myvar['mtr1'][-30:].plot(df['time'][-30:])
+# axes.set_xticklabels(x_values)
 plt.show()
-#- now, play with the zoom to see the y-ticks changing
